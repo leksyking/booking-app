6f1e8c49c5743df9fc001ce6b84ceaaf44352c71
Leksyking <gbemilekeogundipe@gmail.com>
2022-04-01 16:42:01 -0700

    add remaining tickets logic

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	conferenceName := "Go Conference"
 	const conferenceTickets int = 50
-	remainingTickets := 50
+	var remainingTickets uint = 50
 
 	fmt.Printf("conference Ticket is %T, remaining Ticket is %T and conferenceName is %T.\n", conferenceTickets, remainingTickets, conferenceName)
 	fmt.Printf("Welcome to our %v booking application\n", conferenceName)
@@ -15,7 +15,7 @@ func main() {
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets int
+	var userTickets uint
 	//ask users for their name
 	fmt.Println("Enter your first name: ")
 	fmt.Scan(&firstName)
@@ -29,5 +29,8 @@ func main() {
 	fmt.Println("Enter number of tickets: ")
 	fmt.Scan(&userTickets)
 
+	remainingTickets -= userTickets
+
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v", remainingTickets, conferenceName)
 }
