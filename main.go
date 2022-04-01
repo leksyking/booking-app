594b4a4e1b2c9390dc4989a8988850b954fd5126
Leksyking <gbemilekeogundipe@gmail.com>
2022-04-01 15:48:28 -0700

    collect user details and print a result to them

--- main.go
+++ main.go
@@ -3,6 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Welcome to our conference booking application")
+	conferenceName := "Go Conference"
+	const conferenceTickets int = 50
+	remainingTickets := 50
+
+	fmt.Printf("conference Ticket is %T, remaining Ticket is %T and conferenceName is %T.\n", conferenceTickets, remainingTickets, conferenceName)
+	fmt.Printf("Welcome to our %v booking application\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
+	//ask users for their name
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 }
