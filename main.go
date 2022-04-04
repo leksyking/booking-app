0f9d96a1778f1c51d35fd7fb3d80c8f48c0ba3b0
Leksyking <gbemilekeogundipe@gmail.com>
2022-04-03 18:36:56 -0700

    add helper folder

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"strings"
+
+	"github.com/leksyking/booking-app/helper"
 )
 
 const conferenceTickets int = 50
@@ -19,7 +21,7 @@ func main() {
 		//get user inputs
 		firstName, lastName, email, userTickets := getUserInput()
 		//validate user inputs
-		isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets)
+		isValidName, isValidEmail, isValidTicketNumber := helper.ValidateUserInput(firstName, lastName, email, userTickets, remainingTickets)
 
 		if isValidEmail && isValidName && isValidTicketNumber {
 			//book Ticket
@@ -62,13 +64,6 @@ func getFirstNames() []string {
 		firstNames = append(firstNames, names[0])
 	}
 	return firstNames
-}
-
-func validateUserInput(firstName, lastName, email string, userTickets uint) (bool, bool, bool) {
-	isValidName := len(firstName) > 2 && len(lastName) > 2
-	isValidEmail := strings.Contains(email, "@")
-	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
-	return isValidName, isValidEmail, isValidTicketNumber
 }
 
 func getUserInput() (string, string, string, uint) {
